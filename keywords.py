ed56830055163c7ba57d2426e109765fa437e087
Tamuno opubo Dappa <dappatamunoopubo@gmail.com>
2025-07-04 20:03:58 +0100

    Delete keywords.py

--- keywords.py
+++
@@ -1,6 +0,0 @@
-import yake
-
-def extract_keywords(text):
-    kw_extractor = yake.KeywordExtractor(top=5, stopwords=None)
-    keywords = kw_extractor.extract_keywords(text)
-    return [kw for kw, _ in keywords]
