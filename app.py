5d7ce3c8aad8f2c789cb905ad9c86492da652947
Tamuno opubo Dappa <dappatamunoopubo@gmail.com>
2025-07-07 20:38:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from summarize import summarize_article
 from categories import categorize
 
 st.set_page_config(page_title="News Summarizer", layout="wide")
-st.title("📰 AI News Aggregator & Summarizer")
+st.title("AI News Aggregator & Summarizer")
 
 st.sidebar.title("Options")
 refresh = st.sidebar.button("🔄 Refresh News")
